ccb545bbbbf076ad5301562d86b39e77c33562b6
HuynhSang2005 <huynhsang060305@gmail.com>
2025-07-16 18:38:18 +0700

    chore: refine job validator logic for improved data validation

--- src/api/validators/job.validator.ts
+++ src/api/validators/job.validator.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-// Định nghĩa schema cho body của request khi submit job
+// define schema cho body của request khi submit job
 export const submitJobSchema = z.object({
   body: z.object({
     text: z.string()

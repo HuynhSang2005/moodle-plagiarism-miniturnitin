e16c09cfc88e2af050b08b628cfd25bd9942362a
HuynhSang2005 <huynhsang060305@gmail.com>
2025-07-16 22:46:34 +0700

    feat(plugin): initialize basic Moodle plugin structure

---
+++ moodle-dev/miniturnitin/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'plagiarism_miniturnitin';
+$plugin->version   = 2024071600; // Phiên bản YYYYMMDDXX
+$plugin->requires  = 2022041200;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = 'v1.0-alpha';
